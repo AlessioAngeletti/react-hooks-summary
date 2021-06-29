cc6fdc868cc80579da2e3b0c3717b8186274324f
Alessio Angeletti <alessioangeletti@libero.it>
2021-06-29 11:17:28 +0200

    useEffect(), Loading Data & useCallback()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,34 @@ import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     'https://react-http-cb431-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json'
+  //   )
+  //     .then((res) => res.json())
+  //     .then((resData) => {
+  //       const loadedIngredients = [];
+
+  //       for (const key in resData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: resData[key].title,
+  //           amount: resData[key].amount,
+  //         });
+  //       }
+
+  //       setUserIngredients(loadedIngredients);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS');
+  });
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
@@ -32,7 +60,7 @@ function Ingredients() {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    const query =
+      enteredFilter.length === 0
+        ? ''
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+
+    fetch(
+      'https://react-http-cb431-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' +
+        query
+    )
+      .then((res) => res.json())
+      .then((resData) => {
+        const loadedIngredients = [];
+
+        for (const key in resData) {
+          loadedIngredients.push({
+            id: key,
+            title: resData[key].title,
+            amount: resData[key].amount,
+          });
+        }
+
+        onLoadIngredients(loadedIngredients);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
-    <section className="search">
+    <section className='search'>
       <Card>
-        <div className="search-input">
+        <div className='search-input'>
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type='text'
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
